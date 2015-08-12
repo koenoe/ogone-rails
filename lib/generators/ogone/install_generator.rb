d979b32ae0fd50768ad0fa6b510fec6ad050de79
Koen Romers <info@koenromers.com>
2015-08-12 12:04:55 +0200

    fix(generator): renaming and restructuring

---
+++ lib/generators/ogone/install_generator.rb
@@ -0,0 +1,15 @@
+require "rails/generators/base"
+
+module Ogone
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_files
+        copy_file "ogone.yml", "config/ogone.yml"
+        copy_file "ogone.rb", "config/initializers/ogone.rb"
+      end
+    end
+  end
+end
